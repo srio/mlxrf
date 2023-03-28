5333745b664e10d2804ccf1339e47ea5712e1c7a
Manuel Sanchez del Rio <srio@esrf.eu>
2023-03-28 11:58:46 +0200

    first commit

---
+++ mlxrf/create_training_set.py
@@ -0,0 +1,172 @@
+
+from mlcrl.phasenet.zernike import Zernike
+import numpy
+from srxraylib.plot.gol import plot, plot_table
+import matplotlib.pyplot as plt
+
+def run_tubes(ITUBE=2, # 0=Mo, 1=Rh, 2= W
+              VOLTAGE=30, # kV
+              do_plot=0,
+              ):
+
+    anode = ['Mo','Rh','W'][ITUBE]
+    if VOLTAGE <=18 or VOLTAGE >=42:
+        raise(Exception("Voltage (%f) must be 18<E<42" % VOLTAGE))
+
+    #
+    # script to make the calculations (created by XOPPY:xtubes)
+    #
+    import numpy
+    import scipy.constants as codata
+    from xoppylib.xoppy_run_binaries import xoppy_calc_xtubes
+
+    out_file =  xoppy_calc_xtubes(
+            ITUBE    = ITUBE,
+            VOLTAGE = VOLTAGE,
+            )
+    data = numpy.loadtxt(out_file)
+
+    # spectral_power = flux / 0.5e-3 * energy * codata.e # W/eV/mA/mm^2(@?m)
+    # cumulated_power = spectral_power.cumsum() * numpy.abs(energy[1]-energy[0]) # W/mA/mm^2(@1m)
+
+    # data to pass to power
+
+    energy = data[:,0]
+    flux = data[:,1] # for units, see discussion in 'help'
+
+    energy_flag = numpy.ones_like(energy)
+    energy_flag[energy < 1000] = 0
+    energy_flag[energy > 40000] = 0
+    # print(energy_flag)
+
+    energy = energy[numpy.where(energy_flag>0)]
+    flux = flux[numpy.where(energy_flag>0)]
+
+    # print(energy)
+    #
+    # example plot
+    #
+    if do_plot:
+        from srxraylib.plot.gol import plot
+
+        plot(energy,flux,
+            xtitle="Photon energy [eV]",ytitle="Fluence [photons/s/mm^2/0.5keV(bw)/mA]",
+             title="xtubes Fluence; anode=%s,  V=%g kV, NPOINTS=%d" % (anode, VOLTAGE, energy.size),
+            xlog=False,ylog=False,show=True)
+        # plot(energy,spectral_power,
+        #     xtitle="Photon energy [eV]",ytitle="Spectral Power [W/eV/mA/mm^2(@?m) ]",title="xtube_w Spectral Power",
+        #     xlog=False,ylog=False,show=False)
+        # plot(energy,cumulated_power,
+        #     xtitle="Photon energy [eV]",ytitle="Cumulated Power [W/mA/mm^2(@?m) ]",title="xtube_w Cumulated Power",
+        #     xlog=False,ylog=False,show=True)
+
+    #
+    # end script
+    #
+    return energy.copy(), flux.copy()
+
+def get_xrf(ITUBE=2, # 0=Mo, 1=Rh, 2= W
+              VOLTAGE=30, # kV
+              do_plot=0,
+              ):
+
+    spectrum_e, spectrum_f = run_tubes(ITUBE=ITUBE, VOLTAGE=VOLTAGE, do_plot=do_plot)
+
+
+    for i, energy in enumerate(spectrum_e):
+        filename = "xrf_axo_%s_keV.csv" % repr(numpy.round(energy * 1e-3, 5))
+        # print("filename: ", dir_path + filename)
+        a = numpy.loadtxt(dir_path + filename, delimiter=',', skiprows=1)
+        # print(a.shape)
+        if do_plot > 1: plot(a[:,0], a[:,1], title="%s" % energy)
+        if i == 0:
+            xrf_e = a[:, 0]
+            xrf_fcte = a[:, 1]
+            xrf_f = spectrum_f[i] * a[:, 1]
+        else:
+            xrf_e = a[:, 0]
+            xrf_fcte =+ a[:, 1]
+            xrf_f += spectrum_f[i] * a[:, 1]
+
+    return spectrum_e, spectrum_f,  xrf_e, xrf_f, xrf_fcte
+
+
+if __name__ == "__main__":
+
+    from srxraylib.plot.gol import plot
+    #
+    # retrieve and plot 1d (vertical) Zernike profiles
+    #
+
+    # Para cada energía utilicé 1e10 ph/s y un tiempo de colección del espectro de 100 ms.
+
+
+    if False:
+        dir_path = "/tmp_14_days/reyesher/to_Manolo/XRF_spectra/"
+
+        energies = numpy.arange(1.5, 41, 0.01)
+        energies = numpy.arange(1.5, 41, 1)
+        print(energies)
+
+        for energy in energies:
+            filename = "xrf_axo_%s_keV.csv" % repr(energy)
+            print("filename: ", dir_path + filename)
+            a = numpy.loadtxt(dir_path + filename, delimiter=',', skiprows=1)
+            print(a.shape)
+            plot(a[:,0], a[:,1], title="%s" % energy)
+
+        e, f = run_tubes(ITUBE=0, VOLTAGE=20, do_plot=1)
+
+
+    if False:
+        dir_path = "/tmp_14_days/reyesher/to_Manolo/XRF_spectra/"
+
+        ITUBE = 2
+        VOLTAGE = 40
+
+        spectrum_e, spectrum_f, xrf_e, xrf_f, xrf_fcte = get_xrf(ITUBE=ITUBE, VOLTAGE=VOLTAGE, do_plot=0)
+
+        plot(spectrum_e, spectrum_f,
+             title="ITUBE=%d, VOLTAGE=%f" % (ITUBE, VOLTAGE), legend=["tube spectrum N=%d" % spectrum_e.size])
+
+        plot(xrf_e, xrf_f,
+             xrf_e, xrf_fcte / xrf_fcte.max() * xrf_f.max(),
+             title="ITUBE=%d, VOLTAGE=%f, N=%d" % (ITUBE, VOLTAGE, xrf_e.size), legend=["with spectrum", "with cte spectrum"])
+
+
+    if True:
+
+        dir_path = "/tmp_14_days/reyesher/to_Manolo/XRF_spectra/"
+
+        nsamples = 1000
+
+        # X1 = numpy.random.randint(low=0, high=3, size=(nsamples,))
+        X1 = numpy.ones(nsamples, dtype=int) * 2
+        # print(X1)
+
+        print(len(X1[numpy.where(X1 == 0)]))
+        print(len(X1[numpy.where(X1 == 1)]))
+        print(len(X1[numpy.where(X1 == 2)]))
+
+        Y1 = numpy.random.rand(nsamples) * (42-18) + 18
+
+        for i in range(nsamples):
+            fileroot = "./data1/sampled_%05d" % (i)
+            print(i, fileroot)
+
+            spectrum_e, spectrum_f, xrf_e, xrf_f, xrf_fcte = get_xrf(ITUBE=X1[i], VOLTAGE=Y1[i], do_plot=0)
+
+            numpy.savetxt(fileroot + "_spe.dat",
+                          numpy.column_stack((spectrum_e, spectrum_f)) )
+
+            numpy.savetxt(fileroot + "_xrf.dat",
+                          numpy.column_stack((xrf_e, xrf_f, xrf_fcte)) )
+
+            f = open(fileroot + ".txt", 'w')
+            f.write("%d  %g\n" % (X1[i], Y1[i]))
+            f.close()
+
+
+
+
+
---
+++ mlxrf/training.py
@@ -0,0 +1,287 @@
+
+
+import numpy
+from mlcrl.get_wofry_data import get_wofry_data
+from tensorflow.keras import layers
+from tensorflow.keras import models
+
+from srxraylib.plot.gol import plot
+
+from keras.models import load_model
+import pickle
+
+from keras.optimizers import RMSprop
+import json
+
+from srxraylib.plot.gol import plot_image
+
+def get_model(
+    architecture = "convnet", # not used!
+    # kernel_size = (3, 3),
+    # pool_size = (2, 2),
+    kernel_size=(3, 1),
+    pool_size=(2, 1),
+    activation = 'relu', # 'tanh', #  'softmax'
+    padding = 'same',
+    # input_shape = tuple((256, 64, 1)),
+    # output_size = 7,
+    input_shape=tuple((8101, 1)),
+    output_size=87,
+    ):
+
+    model = models.Sequential()
+
+    model.add(layers.Conv2D(8, name='conv1', kernel_size=kernel_size, activation=activation, padding=padding,
+                            input_shape=input_shape))
+    model.add(layers.Conv2D(8, name='conv2', kernel_size=kernel_size, activation=activation, padding=padding))
+    model.add(layers.MaxPooling2D(name='maxpool1', pool_size=pool_size))
+
+    model.add(layers.Conv2D(16, name='conv3', kernel_size=kernel_size, activation=activation, padding=padding))
+    model.add(layers.Conv2D(16, name='conv4', kernel_size=kernel_size, activation=activation, padding=padding))
+    model.add(layers.MaxPooling2D(name='maxpool2', pool_size=pool_size))
+
+    model.add(layers.Conv2D(32, name='conv5', kernel_size=kernel_size, activation=activation, padding=padding))
+    model.add(layers.Conv2D(32, name='conv6', kernel_size=kernel_size, activation=activation, padding=padding))
+    model.add(layers.MaxPooling2D(name='maxpool3', pool_size=pool_size))
+
+    model.add(layers.Conv2D(64, name='conv7', kernel_size=kernel_size, activation=activation, padding=padding))
+    model.add(layers.Conv2D(64, name='conv8', kernel_size=kernel_size, activation=activation, padding=padding))
+    model.add(layers.MaxPooling2D(name='maxpool4', pool_size=pool_size))
+
+    model.add(layers.Conv2D(128, name='conv9', kernel_size=kernel_size, activation=activation, padding=padding))
+    model.add(layers.Conv2D(128, name='conv10', kernel_size=kernel_size, activation=activation, padding=padding))
+    try:
+        if input_shape[0] == 1:
+            model.add(layers.MaxPooling2D(name='maxpool5', pool_size=(1, 2)))
+        else:
+            model.add(layers.MaxPooling2D(name='maxpool5', pool_size=(2, 2)))
+    except:
+        model.add(layers.MaxPooling2D(name='maxpool5', pool_size=(1, 1)))
+
+    model.add(layers.Flatten(name='flat'))
+    model.add(layers.Dense(64, name='dense1', activation=activation))
+    model.add(layers.Dense(64, name='dense2', activation=activation))
+    model.add(layers.Dense(output_size, name='Y', activation='linear'))
+
+    print(model.summary())
+    return model
+
+def load_training_set(nsamples=30, verbose=1, nbin=1, training_ratio=2/3):
+    a = numpy.loadtxt("./data1/sampled_00000_spe.dat")
+    ashape = a.shape
+    targets = numpy.zeros((nsamples, a.shape[0]))
+
+    b = numpy.loadtxt("./data1/sampled_00000_xrf.dat")
+    bshape = b.shape
+    data = numpy.zeros((nsamples, b.shape[0], 1, 1))
+    #                                         ^ fake, to use Conv2D
+    #                                            ^ channels
+
+    print(ashape, bshape)
+
+    for i in range(nsamples):
+        a = numpy.loadtxt("./data1/sampled_%05d_spe.dat" % i)
+        b = numpy.loadtxt("./data1/sampled_%05d_xrf.dat" % i)
+        if a.shape != ashape: raise Exception("Bad dimensions in ./data1/sampled_%05d_spe.dat" % i)
+        if b.shape != bshape: raise Exception("Bad dimensions in ./data1/sampled_%05d_xrf.dat" % i)
+
+        targets[i, :] = a[:, 1]
+        data[i, :, 0, 0] = b[:, 1]
+
+    # from srxraylib.plot.gol import plot_image
+    # plot_image(targets[:,:,0], numpy.arange(nsamples), a[:, 0], aspect='auto', xtitle='nsamples', ytitle='E/eV', title='source spectra')
+    # plot_image(data[:,:,0,0], numpy.arange(nsamples), b[:, 0], aspect='auto', xtitle='nsamples', ytitle='E/keV', title='xrf spectra')
+
+    if verbose: print(targets.shape, targets)
+
+    # h5_file = "%s%s_block.h5" % (dir_out, root)
+    #
+    # f = h5py.File(h5_file, 'r')
+    #
+    # data = f['allsamples/intensity/stack_data'][:]
+    #
+    # f.close()
+
+    if verbose: print(data.shape, data)
+
+    size_data = data.shape[0]
+    size_target = targets.shape[0]
+
+    if size_data != size_target:
+        raise Exception("Data and targets must have the same size.")
+
+
+    istart_training = 0
+    iend_training = int(training_ratio * size_data)
+    istart_test = iend_training
+    iend_test = size_data
+
+    print(">>>>> iend_training", iend_training)
+    if nbin > 0: # normal binning
+        return (data[istart_training:iend_training,:,::nbin].copy(), targets[istart_training:iend_training].copy()), \
+               (data[istart_test:iend_test,:,::nbin].copy(), targets[istart_test:iend_test].copy())
+    else: # take the last part
+        return (data[istart_training:iend_training,:,(data.shape[-1]//numpy.abs(nbin)):].copy(), targets[istart_training:iend_training].copy()), \
+           (data[istart_test:iend_test,:,(data.shape[-1]//numpy.abs(nbin)):].copy(), targets[istart_test:iend_test].copy())
+
+
+if __name__ == "__main__":
+
+    (training_data, training_target), (test_data, test_target) = load_training_set(nsamples=30,
+                                                                                   verbose=1,
+                                                                                   nbin=1) # !!!!!!!!!!!!!! binning  !!!!!!!!!!!
+
+    print("shape of Training: ", training_data.shape, training_target.shape)
+    print("shape of Test: ", test_data.shape, test_target.shape)
+
+
+
+    # plot_image(training_target[:,:], numpy.arange(training_target.shape[0]), numpy.arange(training_target.shape[1]), aspect='auto', xtitle='nsamples', ytitle='index of E/eV', title='source spectra')
+    # plot_image(training_data[:,:,0,0], numpy.arange(training_data.shape[0]), numpy.arange(training_data.shape[1]), aspect='auto', xtitle='nsamples', ytitle='index of E/keV', title='xrf spectra')
+
+    # # data type: images— 4D tensors of shape (samples, height, width, channels) or (samples, channels, height, width)
+    # #            could also be Timeseries data or sequence data— 3D tensors of shape (samples, timesteps, features)
+    # #            right now our data is (samples, features (256), timesteps (65))
+    # training_data = training_data.reshape((training_data.shape[0], training_data.shape[1], training_data.shape[2], 1))
+
+    #
+    min_training_data = training_data.min()
+    max_training_data = training_data.max()
+    print("Min, Max of Training: ", min_training_data, max_training_data)
+
+
+    # normalization
+    training_data = training_data.astype('float32')
+    training_data = (training_data - min_training_data) / (max_training_data - min_training_data)
+
+    test_data = test_data.reshape((test_data.shape[0], test_data.shape[1], test_data.shape[2], 1))
+    test_data = test_data.astype('float32')
+    test_data = (test_data - min_training_data) / (max_training_data - min_training_data)
+
+    #
+    #
+    #
+    # # train_labels = to_categorical(train_labels)
+    # # test_labels = to_categorical(test_labels)
+    #
+    #
+    #
+    #
+    do_train = 0
+    model_root = "training_v01"
+
+    if do_train:
+        input_shape = tuple((training_data.shape[1], training_data.shape[2], training_data.shape[3]))
+        output_size = training_target.shape[1]
+        print("input shape: ", input_shape)
+        print("output shape: ", output_size)
+
+        model = get_model(input_shape=input_shape, output_size=output_size)
+
+        # To perform regression toward a vector of continuous values, end your stack of layers
+        # with a Dense layer with a number of units equal to the number of values you’re trying
+        # to predict (often a single one, such as the price of a house), and no activation. Several
+        # losses can be used for regression, most commonly mean_squared_error ( MSE ) and
+        # mean_absolute_error ( MAE )
+
+        # to choose thecorrect loss:
+        # For instance, you’ll use binary crossentropy for a two-class classification
+        # problem, categorical crossentropy for a many-class classification problem, mean-
+        # squared error for a regression problem, connectionist temporal classification ( CTC )
+        # for a sequence-learning problem, and so on.
+
+        model.compile(
+                      # optimizer='rmsprop',
+                      optimizer=RMSprop(lr=1e-4),
+                      loss='mse',
+                      # metrics=['mae'], # mean absolute error
+                      # loss='categorical_crossentropy',
+                      metrics=['accuracy'],
+                      )
+
+        # filename = 'training_v1.csv'
+        # import tensorflow as tf
+        # history_logger = tf.keras.callbacks.CSVLogger(filename, separator=" ", append=False)
+
+
+        history = model.fit(training_data, training_target,
+                            epochs=1500, batch_size=64, validation_split=0.2,
+                            # callbacks=[history_logger],
+                            )
+
+        model.save('%s.h5' % model_root)
+
+
+        history_dict = history.history
+
+
+        with open("%s.json" % model_root, "w") as outfile:
+            json.dump(history_dict, outfile)
+
+    else:
+        dir_out = "./"
+        model = load_model('%s/%s.h5' % (dir_out, model_root))
+
+        f = open("%s/%s.json" % (dir_out, model_root), "r")
+        f_txt = f.read()
+        history_dict = json.loads(f_txt)
+
+    print(history_dict.keys())
+
+    import matplotlib.pyplot as plt
+    loss_values = history_dict['loss']
+    val_loss_values = history_dict['val_loss']
+    epochs = range(1, len(loss_values) + 1)
+    plot(epochs, loss_values,
+         epochs, val_loss_values,
+         legend=['loss','val_loss'], xtitle='Epochs', ytitle='Loss', show=0)
+
+    # mae_values = history_dict['mae']
+    # val_mae_values = history_dict['val_mae']
+    # plot(epochs, mae_values,
+    #      epochs, val_mae_values,
+    #      legend=['mae','val_mae'], xtitle='Epochs', ytitle='mae')
+
+    acc_values = history_dict['accuracy']
+    val_acc_values = history_dict['val_accuracy']
+    plot(epochs, val_acc_values,
+         epochs, acc_values,
+         legend=['accuracy on validation set','accuracy on training set'],
+         color=['g','b'], xtitle='Epochs', ytitle='accuracy')
+
+
+    plt.plot(epochs, loss_values, 'bo', label='Training loss')
+    # plt.plot(epochs, mae_values, 'b', label='mae')
+    plt.title('Training')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.show()
+
+
+    #
+    # test evaluation
+    #
+    test_loss, test_acc = model.evaluate(test_data, test_target)
+    #
+    print(test_acc)
+
+
+    #
+    # predictions
+
+    predictions = model.predict(test_data)
+    print(predictions.shape)
+
+    plot_image(predictions, title="predictions", aspect='auto', show=0)
+
+    plot_image(test_target, title="test target", aspect='auto', show=1)
+
+    for i in range(test_target.shape[0]):
+        plot(numpy.arange(predictions.shape[1]), predictions[i],
+             numpy.arange(test_target.shape[1]), test_target[i],
+             legend=["predicted", "target"], title="index=%d" % i)
+
+
+
+
